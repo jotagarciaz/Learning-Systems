efd2dc62ebfd4282a5502dbe0b42ed21c5b9cb6a
Clare <claretgb@gmail.com>
2019-02-07 10:01:39 +0100

    We read all the data from the data.txt file.

--- assigment 1/ann.py
+++ assigment 1/ann.py
@@ -5,26 +5,58 @@
         - Clara Torre García-Barredo.
     Code created on February 2019.
 """
+
+f = open("assigment 1/data",'r')
+f.seek(0)
+n_lines_to_read = len(f.readlines())
+
 def num(s):
     try:
         return int(s)
     except ValueError:
         return float(s)
     
-def read_training_set(file):
-    f = open(file,'r')
+def read_training_set():
+    global f, n_lines_to_read
+    n_lines_training = int(0.75 * n_lines_to_read)
+    training_set = [] #type: list
     f.seek(0)
-    n_lines_to_read = len(f.readlines())
-    n_lines_to_read = int(0.75 * n_lines_to_read)
-    training_set = []
-    f.seek(0)
-    for i in range(0,n_lines_to_read):
+    for i in range(0,n_lines_training):
         aux = f.readline()
         training_set.append(aux.split(",",19))
 
     for i in range(0,len(training_set)):
         training_set[i] = list(map(lambda x: num(x), training_set[i]))
    
-    print(training_set)
+    return training_set
+
+def read_validation_set():
+    global f, n_lines_to_read
+    n_lines_validation = int(0.10 * n_lines_to_read)
+    validation_set = [] #type: list
+    for i in range(0, n_lines_validation):
+        aux = f.readline()
+        validation_set.append(aux.split(",", 19))
     
-read_training_set("assigment 1/data")
+    for i in range(0, len(validation_set)):
+        validation_set[i] = list(map(lambda x: num(x), validation_set[i]))
+    
+    return validation_set
+
+def read_testing_set():
+    global f, n_lines_to_read
+    n_lines_testing = int(0.15 * n_lines_to_read + 1)
+    testing_set = [] #type: list
+    for i in range(0, n_lines_testing):
+        aux = f.readline()
+        testing_set.append(aux.split(",", 19))
+    
+    for i in range(0, len(testing_set)):
+        testing_set[i] = list(map(lambda x: num(x), testing_set[i]))
+    
+    return testing_set
+    
+# First we get the data from the data.txt file.
+training_set = read_training_set()
+validation_set = read_validation_set()
+testing_set = read_testing_set()
