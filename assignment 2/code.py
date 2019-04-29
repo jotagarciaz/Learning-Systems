865d3dbdbb5abc064070f7547d73d4c2a199e7e2
Clare <claretgb@gmail.com>
2019-04-29 19:44:15 +0200

    Works up to 70% accuracy.

--- assignment 2/code.py
+++ assignment 2/code.py
@@ -15,8 +15,14 @@ f = open("/Users/clara/Documents/GitHub/Learning-Systems/assignment 2/iris", 'r'
 f.seek(0)
 n_lines_to_read = len(f.readlines())
 LINE_LEN=5
-ACCURACY_REQUIRED = 0.9
-MIDDLE=0.6
+
+MIDDLE=0.25
+s = 0
+m = 1
+l = 2
+SETOSA = 1
+VERSICOLOR = 2
+VIRGINICA = 3
 
 def num(s):
 	try:
@@ -73,7 +79,7 @@ def main():
 			if element[i] <= MIDDLE:
 				middle = element[i]/MIDDLE
 			else:
-				middle = (element[i]-MIDDLE)/(1-MIDDLE)
+				middle = 1 - (element[i]-MIDDLE)/(1-MIDDLE)
 			if middle < 0:
 				middle = 0
 			x_values_row.append(middle)
@@ -85,102 +91,27 @@ def main():
 			x_values.append(x_values_row)
 
 		#Calculate the truth values of the rules.
-		x1 = x_values[0].index(max(x_values[0]))
-		x2 = x_values[1].index(max(x_values[1]))
-		x3 = x_values[2].index(max(x_values[2]))
-		x4 = x_values[3].index(max(x_values[3]))
 		truth_values = [] #type: list
-		truth1 = 0
 		#Rule 1:
-		if x1 == 0:
-			truth1 += 1 - (element[0]/MIDDLE)
-			if x2 == 1 and element[1] <= MIDDLE:
-				truth1 += element[1]/MIDDLE
-				if x3 == 1 and element[2] <= MIDDLE:
-					truth1 += element[2]/MIDDLE
-					if x4 == 1 and element[3] <= MIDDLE:
-						truth1 += element[3]/MIDDLE
-					elif x4 == 1:
-						truth1 += (element[3]-MIDDLE)/(1-MIDDLE)
-				elif x3 == 1 or x3 == 2:
-					truth1 += (element[2]-MIDDLE)/(1-MIDDLE)
-			elif x2 == 1 or x2 == 2:
-				truth1 +=(element[1]-MIDDLE)/(1-MIDDLE)
-		elif x1 == 2:
-			truth1 += (element[0]-MIDDLE)/(1-MIDDLE)
-
-		truth_values.append(truth1/4)
-		
-		truth2 = 0
-		#Rule 2:
-		if x3 == 0:
-			truth2 += 1 - (element[2]/MIDDLE)
-			if x4 == 0:
-				truth2 += element[3]/MIDDLE
-		elif x3 == 1 and element[2] <= MIDDLE:
-			truth2 += element[2]/MIDDLE
-			if x4 == 0:
-				truth2 += element[3]/MIDDLE
-		elif x3 == 1:
-			truth2 += (element[2]-MIDDLE)/(1-MIDDLE)
-		
-		truth_values.append(truth2/2)
-
-		truth3 = 0
-		#Rule 3:
-		if x2 == 0:
-			truth3 += 1 - (element[1]/MIDDLE)
-			if x3 == 2:
-				truth3 += (element[2]-MIDDLE)/(1-MIDDLE)
-				if x4 == 2:
-					truth3 += (element[3]-MIDDLE)/(1-MIDDLE)
-		elif x2 == 1 and element[1] <= MIDDLE:
-			truth3 += element[1]/MIDDLE
-			if x3 == 2:
-				truth3 += (element[2]-MIDDLE)/(1-MIDDLE)
-				if x4 == 2:
-					truth3 += (element[3]-MIDDLE)/(1-MIDDLE)	
-		elif x2 == 1:
-			truth3 +=(element[1]-MIDDLE)/(1-MIDDLE)
-
-		truth_values.append(truth3/3)
-
-		truth4 = 0
-		#Rule 4:
-		if x1 == 1 and element[0] <= MIDDLE:
-			truth4 += element[0]/MIDDLE
-			if x2 == 0:
-				truth4 += 1 - (element[1]/MIDDLE)
-				if x3 == 0:
-					truth4 += 1 - (element[2]/MIDDLE)
-					if x4 == 2:
-						truth4 += (element[3]-MIDDLE)/(1-MIDDLE)
-			elif x2 == 1 and element[1] <= MIDDLE:
-				truth4 += element[1]/MIDDLE
-				if x3 == 0:
-					truth4 += 1 - (element[2]/MIDDLE)
-					if x4 == 2:
-						truth4 += (element[3]-MIDDLE)/(1-MIDDLE)
-			elif x2 == 1:
-				truth4 +=(element[1]-MIDDLE)/(1-MIDDLE)
-		elif x1 == 1:
-			truth4 += (element[0]-MIDDLE)/(1-MIDDLE)
-			
-		truth_values.append(truth4/4)
+		truth_values.append((x_values[0][s] + x_values[0][l])*(x_values[1][m] + x_values[1][l])*(x_values[2][m] + x_values[2][l])*x_values[3][m])
+		truth_values.append((x_values[2][s] + x_values[2][m])*x_values[3][s])
+		truth_values.append((x_values[1][s] + x_values[1][m])*x_values[2][l]*x_values[3][l])
+		truth_values.append(x_values[0][m]*(x_values[1][s] + x_values[1][m])*x_values[2][s]*x_values[3][l])
 
 		rule = truth_values.index(max(truth_values))
+
 		if rule == 0 or rule == 3:
-			output = 2
+			output = VERSICOLOR
 			if result_e == output:
 				hits[1] += 1
 			counter[1] += 1
-		if rule == 1:
-			output = 1
+		elif rule == 1:
+			output = SETOSA
 			if result_e == output:
 				hits[0] += 1
 			counter[0] += 1
-		if rule == 2: 
-			output = 3
+		elif rule == 2: 
+			output = VIRGINICA
 			if result_e == output:
 				hits[2] += 1
 			counter[2] += 1  
